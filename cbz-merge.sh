ae66f63004c2cb58b68d79c65d7e24e7d0fd7930
LiamAEdwards <liam.a.edwards@protonmail.com>
2021-12-18 06:31:01 +0000

    Added user flags

--- cbz-merge.sh
+++ cbz-merge.sh
@@ -1,12 +1,50 @@
 #!/bin/bash
 
-echo "Enter absolute path to the directory you wish cbz-merge to run. - Example /home/USER/directory/subdirectory"
-read directory
-basedir=$(basename $directory)
+# Read directory input
+echo $1 $2
+cd $2
 
-echo $directory
-unzip "$directory/*.cbz" -d "$directory" | tee -a cbz-merge.log
-zip -r "$directory/$basedir.cbz" "$directory/" -x "*.cbz" | tee -a cbz-merge.log
 
-# Convert cbz for e-reader
-ebook-convert $directory/$basedir.cbz $directory/$basedir.mobi | tee -a cbz-merge.log
+if [ $# -eq 0 ]; then
+        echo "No arguments specified"       
+	exit 1
+fi
+
+if [ $2 == ""]; then
+        echo "No directory specified"
+        exit 1
+fi
+basedir=$(basename $2)
+
+while [ "$1" != "" ]; do
+    case $1 in
+    -j | --jpeg)
+        ## START --jpeg LOGS
+        zip -r "$basedir.cbz" * -x "*.cbz" | tee -a cbz-merge.log
+        ## END --jpeg LOGS
+        ;;
+    -h | --help)
+        ## PRINT HELP MENU
+        ;;
+    -z | --zip)
+        ## START --zip LOGS
+        ## UNZIP
+        unzip "*.cbz" -d . | tee -a cbz-merge.log
+        ## ZIP
+        zip -r "$basedir.cbz" * -x "*.cbz" | tee -a cbz-merge.log
+        ## END --zip LOGS
+        break
+        ;;
+    esac
+done
+
+echo "Merge has been complete, do you want to convert to mobi? Y/N"
+read input
+
+if [ $input == "Y" ]; then
+        ebook-convert $basedir.cbz $basedir.mobi | tee -a cbz-merge.log
+else
+        echo "Exiting"
+        exit 1
+fi
+
