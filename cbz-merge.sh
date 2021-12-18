743b888162542cbaa6a0e06e66e53236d71b49b3
LiamAEdwards <liam.a.edwards@protonmail.com>
2021-12-18 01:33:10 +0000

    output file correctly names

--- cbz-merge.sh
+++ cbz-merge.sh
@@ -2,7 +2,8 @@
 
 echo "Enter absolute path to the directory you wish cbz-merge to run. - Example /home/USER/directory/subdirectory"
 read directory
+basedir=$(basename $directory)
 
 echo $directory
 unzip "$directory/*.cbz" -d "$directory"
-zip -r "$directory.cbz" "$directory/" -x "*.cbz"
+zip -r "$directory/$basedir.cbz" "$directory/" -x "*.cbz"
