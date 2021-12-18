712f29e129b6358eb7d7f4e2455fe9b930874d5b
LiamAEdwards <liam.a.edwards@protonmail.com>
2021-12-18 01:08:32 +0000

    Outputs in correct directory now

--- cbz-merge.sh
+++ cbz-merge.sh
@@ -2,9 +2,10 @@
 
 echo "Enter absolute path to the directory you wish cbz-merge to run. - Example /home/USER/directory/subdirectory"
 read directory
-#currentDir=${PWD##*$directory}   
 
-echo $directory
+# For setting the correct name of the output .cbz file
+basedir=$(basename $directory)
+
+# Unzip and zip
 unzip $directory/*.cbz -d $directory 
-zip -r $directory.cbz $directory/* -x "*.cbz"
-
+zip -r $directory/$basedir.cbz $directory/* -x "*.cbz"
